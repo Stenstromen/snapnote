4ed1324d61784e03c1b9b6bf282eba334ae4d69a
Stenstromen <kontakt@filipstenstrom.se>
2023-06-01 09:08:42 +0200

    Updated styling

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -10,21 +10,22 @@ function Sidebar({
   currentId: number;
   setCurrentId: (id: number) => void;
 }) {
+  const iconSize = 28;
   return (
     <div className="sidebar">
       <Nav className="sidebar-content">
         {notes.map(({ id, title }: { id: number; title: string }) => {
           return (
             <Nav.Link
-            className="nav-link-custom"
+              className="nav-link-custom"
               key={id}
               onClick={() => setCurrentId(id)}
               style={{ color: id === currentId ? "Green" : "White" }}
             >
               {title[0] ? (
-                <HiOutlineDocumentText size={22} />
+                <HiOutlineDocumentText size={iconSize} />
               ) : (
-                <HiOutlineDocument size={22} />
+                <HiOutlineDocument size={iconSize} />
               )}
             </Nav.Link>
           );
